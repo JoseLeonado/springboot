10465f7455494200f1c32d64464bacb1ce0f2581
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2021-10-30 21:01:00 -0300

    Projeto criado

---
+++ src/main/java/com/example/demo/configurandospringsecurity/ConfigurandoSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.configurandospringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurandoSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurandoSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/configurandospringsecurity/ConfigurandoSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.configurandospringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurandoSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
